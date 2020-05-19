299c625cb902505e15d66b88893e6e6967cf1106
Sattar <sattarmath2010@gmail.com>
2020-05-18 23:24:50 -0400

    Connection Added

--- admin-dashboard/order-management/connection.php
+++ admin-dashboard/order-management/connection.php
@@ -4,7 +4,7 @@
  *Created on : 24-Oct-2018, 10:00:13 AM
  */
 try {
-    $dbh = new PDO('mysql:host=localhost;dbname=000394648', "000394648", "19811231");
+    $dbh = new PDO('mysql:host=localhost;dbname=bdname', "username", "password");
 } catch (Exception $e) {
     die('Could not connect to DB: ' . $e->getMessage());
 }
